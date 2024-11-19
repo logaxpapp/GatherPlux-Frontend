e86760276e7dd147d2a0998a5db60587efaab43a
HarunAtk <harunaatokolo28@gmail.com>
2024-11-19 14:31:46 +0100

    Gather Plus

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero";
+import Features from "./components/ExploreEvents";
+import Testimonials from "./components/Testimonials";
+
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Features />
+      <Testimonials />
+    </main>
+  );
+}
